856c8220349810494425e695ee1e5ca1211bdb6e
Reymart Bordo <62002257+Mart-007@users.noreply.github.com>
2021-05-28 10:27:43 +0800

    update gc

--- Group_Chat/app.js
+++ Group_Chat/app.js
@@ -6,9 +6,11 @@ const server = app.listen(7000, () => {
 });
 const io = require('socket.io')(server);
 
+app.use(express.urlencoded({extended: true}))
+
 app.use(express.static(__dirname + "/assets"));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.use(session({
     secret: 'alaws lods',
@@ -18,21 +20,27 @@ app.use(session({
 }))
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index.ejs');
 })
 
 io.on('connection', (socket) => {
     socket.on('username', (username) => {
         socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username +' ' + 'join the chat..</i>');
+        io.emit('is_online', '🟢 <i>' + socket.username +' ' + 'join the chat..</i>');
     });
     socket.on('disconnect', (username) => {
         io.emit('is_online', '🔴 <i>' + socket.username + ' ' + 'left the chat..</i>');
     })
     socket.on('chat_message', (message) => {
-        io.emit('chat_message', '<strong>' + socket.username + '<strong>:' + message);
+        io.sockets.emit('chat_message', '<strong>' + socket.username + '<strong>:' + message);
+        
     })
+    // socket.on('send_message', (message) => {
+    //     io.emit('send_message', message);
+    //     io.broacast.emit('send_message', message);
+    // })
 })
+
 
 
 
